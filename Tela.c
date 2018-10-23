e893fce16738362ded36cb29c6fa1ab6012b48ae
Raul404 <42614209+Raul404@users.noreply.github.com>
2018-10-23 12:31:03 -0300

    REQ01

--- Tela.c
+++ Tela.c
@@ -3,9 +3,24 @@
 #include  "Tela.h"
 
 
-void imprimirTabuleiro(Tabuleiro M[]){
-    for(int i = 0; i < 28; i++){
+void imprimirTabuleiro(Tabuleiro M[])
+{
+    for(int i = 0; i < 28; i++)
+    {
         printf("%d | %d", M[i].p->e, M[i].p->d);
         printf("\n");
     }
+}
+
+int numJogadores()
+{
+int numeroJogadores;
+    printf("para jogar com a maquina digite 1 \n\npara jogar com outro jogador digite 2");
+    scanf("%d",&numeroJogadores);
+    while (numeroJogadores<0 && numeroJogadores>2)
+    {
+        printf("numero de jogadores invalido\n\nDigite Novamente");
+        scanf("%d",&numeroJogadores);
+    }
+    return numeroJogadores;
 }
--- Tela.h
+++ Tela.h
@@ -3,5 +3,6 @@
 #include "Dados.h"
 
 void imprimirTabuleiro(Tabuleiro M[]);
+int numJogadores();
 
 #endif
