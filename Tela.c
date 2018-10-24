39c4455a2e80eb24e292a974e08a4f647a546eaf
Raul404 <42614209+Raul404@users.noreply.github.com>
2018-10-24 12:39:59 -0300

    jogarPL

--- Tela.c
+++ Tela.c
@@ -3,12 +3,23 @@
 #include  "Tela.h"
 
 
-void imprimirTabuleiro(Tabuleiro M[])
-{
-    for(int i = 0; i < 28; i++)
-    {
-        printf("%d | %d", M[i].p->e, M[i].p->d);
-        printf("\n");
+
+void imprimirTabuleiro(Tabuleiro M[]){
+    for(int i = 0; i < 28; i++){
+        if(M[i].p != NULL){
+            printf("%d | %d", M[i].p->e, M[i].p->d);
+            printf("\n");
+        }
+    }
+}
+
+void imprimirMao(Tabuleiro mP[]){
+    for(int i = 0; i < 7; i++){
+        if(mP[i].p != NULL){
+            printf("%d | %d", mP[i].p->e, mP[i].p->d);
+            printf("\n");
+        }
+
     }
 }
 
@@ -23,4 +34,15 @@ int numeroJogadores;
         scanf("%d",&numeroJogadores);
     }
     return numeroJogadores;
+}
+
+int JogarPLTela(){
+    int opcaoEscolhida;
+    printf("Certo, vamos as opções.\n");
+
+    printf("para escolher um peça, digite 1\n");
+    printf("para salvar o jogo , digite 2\n");
+    printf("para sair do jogo , digite 3\n");
+    return opcaoEscolhida
+
 }
