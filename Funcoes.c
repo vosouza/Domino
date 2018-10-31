9d6037ddb4391ccbf670858142addf66ec25c4ea
Vinicius Oliveira Souza <=>
2018-10-31 13:24:52 -0200

    Requisito 8 completo

--- Funcoes.c
+++ Funcoes.c
@@ -4,6 +4,7 @@
 #include "stdio.h"
 void organizarPeca(Tabuleiro mp[],int crescente ,int esquerda);
 int agruparPecas(Tabuleiro pecas[]);
+void organizarSoma(Tabuleiro mp[]);
 
 void inicializarJogo(Tabuleiro T[], Tabuleiro M[], Tabuleiro mP1[], Tabuleiro mP2[], int Modo){
 
@@ -13,14 +14,22 @@ void inicializarJogo(Tabuleiro T[], Tabuleiro M[], Tabuleiro mP1[], Tabuleiro mP
 //    imprimirTabuleiro(M);
 
     darMaos(mP1, mP2, M);
-    imprimirMao(mP2);
+    imprimirMao(mP1);
 
-    printf("\n\nxxxxxxx");
+    printf("\n\n");
     organizarPeca(mP1,1,1);
+    organizarSoma(mP1);
     imprimirMao(mP1);
 
 }
+/*
+    Funcão para organizar as peças em ordem crescente ou decrescente
+    considerando o lado esquedo ou direito das peças
 
+    entrada: Tabuleiro mp, crescente(inteiro), esquerda(inteiro)
+    saida: Tabuleiro mp
+
+*/
 void organizarPeca(Tabuleiro mp[],int crescente ,int esquerda){
     int j,k,fim;
     Tabuleiro peca;
@@ -31,6 +40,38 @@ void organizarPeca(Tabuleiro mp[],int crescente ,int esquerda){
            for(j=0;j<fim-1;j++){
                 for(k=0;k<fim-1;k++){
                     if(mp[k].p->e > mp[k+1].p->e){
+                        peca.p = mp[k].p;
+                        mp[k].p = mp[k+1].p;
+                        mp[k+1].p = peca.p;
+                    }
+                }
+           }
+        }else if(esquerda == 0){
+            for(j=0;j<fim-1;j++){
+                for(k=0;k<fim-1;k++){
+                    if(mp[k].p->d > mp[k+1].p->d){
+                        peca.p = mp[k].p;
+                        mp[k].p = mp[k+1].p;
+                        mp[k+1].p = peca.p;
+                    }
+                }
+           }
+        }
+    }else if (crescente == 0){
+        if(esquerda == 1){
+           for(j=0;j<fim-1;j++){
+                for(k=0;k<fim-1;k++){
+                    if(mp[k].p->e < mp[k+1].p->e){
+                        peca.p = mp[k].p;
+                        mp[k].p = mp[k+1].p;
+                        mp[k+1].p = peca.p;
+                    }
+                }
+           }
+        }else if(esquerda == 0){
+            for(j=0;j<fim-1;j++){
+                for(k=0;k<fim-1;k++){
+                    if(mp[k].p->d < mp[k+1].p->d){
                         peca.p = mp[k].p;
                         mp[k].p = mp[k+1].p;
                         mp[k+1].p = peca.p;
@@ -55,6 +96,23 @@ int agruparPecas(Tabuleiro pecas[]){
         pecas[x].p = aux[x].p;
     }
     return i;
+}
+
+void organizarSoma(Tabuleiro mp[]){
+    int j,k,fim;
+    Tabuleiro peca;
+    peca.p = NULL;
+    fim = agruparPecas(mp);
+    for(j=0;j<fim-1;j++){
+        for(k=0;k<fim-1;k++){
+            if(mp[k].p->s < mp[k+1].p->s){
+                peca.p = mp[k].p;
+                mp[k].p = mp[k+1].p;
+                mp[k+1].p = peca.p;
+
+            }
+        }
+    }
 }
 /*
 void JogarPc(Tabuleiro T, Tabuleiro M, Tabuleiro mP1, Tabuleiro mP2){
